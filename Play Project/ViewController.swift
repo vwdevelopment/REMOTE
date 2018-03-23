683f4376966445fe22013c3ae541b1bdc20ecdbd
Unknown <Brian.vanwagner@outlook.com>
2018-03-22 20:50:21 -0700

    Included Boolean
    We added a switch and addition and subtraction if statements

--- Play Project/ViewController.swift
+++ Play Project/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var buttonCount = 0
+   // var buttonCount = 0
     @IBOutlet weak var myLabel: UILabel!
     
+    @IBOutlet weak var topButton: UITextField!
+    
+    @IBOutlet weak var bottomButton: UITextField!
+   
+    @IBOutlet weak var additionSwitch: UISwitch!
     @IBAction func buttonTapped(_ sender: Any) {
+       
+        let addition = additionSwitch.isOn
         
+        if addition{
+            let sum = Double(topButton.text!)! + Double(bottomButton.text!)!
+            
+            myLabel.text = "The sum is ..." + String(sum)
+        }
+        else {
+            let sum = Double(topButton.text!)! - Double(bottomButton.text!)!
+            
+            myLabel.text = "The difference is ..." + String(sum)
+            
+        }
+        
+      
+    /*
         buttonCount = buttonCount + 1
         
         print(buttonCount)
@@ -22,9 +43,9 @@ class ViewController: UIViewController {
         if buttonCount == 10{
         view.backgroundColor = UIColor.red
         
-            myLabel.text = "Brian is cool"}
+            myLabel.text = "Brian is cool"}*/
     }
-    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
